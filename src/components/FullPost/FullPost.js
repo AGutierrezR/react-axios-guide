ad02d8664b836592f63b581a7342bd775bd1d5a1
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-07-27 14:58:20 +0200

    Arreglar el loop infinito del post individual
    Se coloca un condicional para ver chequear:
    
    - Si no tenemos un post cargado en el componente
    - Si el post que tenemos cargado no es distinto al que el que estamos recibiendo

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,10 +10,12 @@ class FullPost extends Component {
 
     componentDidUpdate() {
         if(this.props.id) {
-            axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
-                .then(res => {
-                    this.setState({loadedPost: res.data})
-                })
+            if(!this.state.loadedPost || (this.state.loadedPost && this.props.id !== this.state.loadedPost.id)) {
+                axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                    .then(res => {
+                        this.setState({loadedPost: res.data})
+                    })
+            }
         }
     }
 
