3802e4d60faea24200e2ec31eb482bbd8bb47b0e
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-07-29 13:33:48 +0200

    Agregar interceptors para ejecutar codigo globalmente
    Lo interceptors puede manipular los request y response que se hacen la app
    
    Podemos usarlo para autenticacion y esos menesteres

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(req => {
+  console.log(req)
+  // Edit request config
+  return req
+}, err => {
+  console.log(err);
+  return Promise.reject(err)
+})
+
+axios.interceptors.response.use(res => {
+  console.log(res)
+  // Edit response config
+  return res
+}, err => {
+  console.log(err);
+  return Promise.reject(err)
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
